8710d564bf3553cf7e136f07e989881fd2f2c63d
markugm <sania.dinara22@gmail.com>
2025-10-27 12:38:53 +0700

    .

--- public/index.php
+++ public/index.php
@@ -1,13 +1,90 @@
 <?php include __DIR__ . '/_header.php'; ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    halo
-</body>
-</html>
+
+<section class="mb-12">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="text-3xl font-semibold text-pretty text-black ">Artikel Terbaru</h2>
+        </div>
+        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 sm:mt-6 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <article class="flex max-w-xl flex-col items-start justify-between">
+                <div class="flex items-center gap-x-4 text-xs">
+                    <time datetime="2020-03-16" class="text-gray-600">Mar 16, 2020</time>
+                </div>
+                <div class="group relative grow">
+                    <h3 class="mt-3 text-xl font-semibold text-black group-hover:text-purple-700">
+                        <a href="#">
+                            <span class="absolute inset-0"></span>
+                            Boost your conversion rate
+                        </a>
+                    </h3>
+                    <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                </div>
+                <div class="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-10 rounded-full bg-gray-800" />
+                    <div class="text-sm/6">
+                        <p class="font-semibold text-black">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Michael Foster
+                            </a>
+                        </p>
+                        <p class="text-gray-400">Co-Founder / CTO</p>
+                    </div>
+                </div>
+            </article>
+            <article class="flex max-w-xl flex-col items-start justify-between">
+                <div class="flex items-center gap-x-4 text-xs">
+                    <time datetime="2020-03-16" class="text-gray-600">Mar 16, 2020</time>
+                </div>
+                <div class="group relative grow">
+                    <h3 class="mt-3 text-xl font-semibold text-black group-hover:text-purple-700">
+                        <a href="#">
+                            <span class="absolute inset-0"></span>
+                            Boost your conversion rate
+                        </a>
+                    </h3>
+                    <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                </div>
+                <div class="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-10 rounded-full bg-gray-800" />
+                    <div class="text-sm/6">
+                        <p class="font-semibold text-black">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Michael Foster
+                            </a>
+                        </p>
+                        <p class="text-gray-400">Co-Founder / CTO</p>
+                    </div>
+                </div>
+            </article>
+            <article class="flex max-w-xl flex-col items-start justify-between">
+                <div class="flex items-center gap-x-4 text-xs">
+                    <time datetime="2020-03-16" class="text-gray-600">Mar 16, 2020</time>
+                </div>
+                <div class="group relative grow">
+                    <h3 class="mt-3 text-xl font-semibold text-black group-hover:text-purple-700">
+                        <a href="#">
+                            <span class="absolute inset-0"></span>
+                            Boost your conversion rate
+                        </a>
+                    </h3>
+                    <p class="mt-5 line-clamp-3 text-sm/6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                </div>
+                <div class="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-10 rounded-full bg-gray-800" />
+                    <div class="text-sm/6">
+                        <p class="font-semibold text-black">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Michael Foster
+                            </a>
+                        </p>
+                        <p class="text-gray-400">Co-Founder / CTO</p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
